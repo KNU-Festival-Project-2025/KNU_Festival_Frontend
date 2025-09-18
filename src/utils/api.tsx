6e0d330672d3056c361e3c630f1f0d98bf7f657e
kpss0337 <kpss0337@gmail.com>
2025-09-18 17:22:38 +0900

    test62

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -10,7 +10,10 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = getAccessToken();
   // 로그인/회원가입 요청에는 Authorization 헤더 제외
-  if (token && !config.url?.includes("/login")) {
+  if (token &&
+    !config.url?.includes("/api/auth/login") &&
+    !config.url?.includes("/login")
+) {
     config.headers!["Authorization"] = `Bearer ${token}`;
   }
   return config;
